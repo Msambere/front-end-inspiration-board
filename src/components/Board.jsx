325aed7f77f57118d570d9b0452bf39ed34d63dc
Amber <110812972+Msambere@users.noreply.github.com>
2025-01-05 19:35:32 -0600

    Sorting functionality added for cards

--- src/components/Board.jsx
+++ src/components/Board.jsx
@@ -3,16 +3,45 @@ import { singleBoardAPICall, newCardAPICall } from "../api/api.js";
 import PropTypes from "prop-types";
 import NewCardForm from "./newCardForm.jsx";
 import CardContainer from "./CardContainer.jsx";
+import SortingButtons from "./SortingButtons.jsx";
 
 const Board = ({ boardId }) => {
   const [board, setBoard] = useState(null);
   const [newCardSubmitStatus, setNewCardSubmitStatus] = useState(null);
+  const [sortValue, setSortValue] = useState("Id");
+  const [sortOrder, setSortOrder] = useState("asc");
 
   useEffect(() => {
     singleBoardAPICall(boardId).then((newBoard) => {
       setBoard(newBoard);
     });
   }, [boardId]);
+
+  const sortOptions = ["Id", "Likes", "Alphabetically"];
+
+
+  const sortedCardData = (data) => {
+    switch (sortValue) {
+      case "Id":
+        return data.sort((a, b) => {
+          return sortOrder === "asc" ? a.id - b.id : b.id - a.id;
+        });
+      case "Likes":
+        return data.sort((a, b) => {
+          return sortOrder === "asc" ? a.likes - b.likes : b.likes - a.likes;
+        });
+      case "Alphabetically":
+        return data.sort((a, b) => {
+          return sortOrder === "asc"
+            ? a.text.localeCompare(b.text)
+            : b.text.localeCompare(a.text);
+        });
+      default:
+        return data;
+    }
+  };
+
+
 
   const createNewCard = (board_id, newCardData) => {
     return newCardAPICall(board_id, newCardData).then(() => {
@@ -33,8 +62,10 @@ const Board = ({ boardId }) => {
 
   return (
     <>
-      <CardContainer cardData={board.cards} />
-      <NewCardForm createNewCard={createNewCard} currentBoard={boardId} setSubmitStatus={setNewCardSubmitStatus}/>
+      <h1>{board.title}</h1>
+      <SortingButtons options={sortOptions} setSortValue={setSortValue} setSortOrder={setSortOrder} sortValue={sortValue} sortOrder={sortOrder}/>
+      <CardContainer cardData={sortedCardData(board.cards)} sortValue={sortValue} sortOrder={sortOrder} />
+      <NewCardForm createNewCard={createNewCard} currentBoard={boardId} setSubmitStatus={setNewCardSubmitStatus} />
       <dialog id="cardSubmitStatus">
         {newCardSubmitStatus}
         <button onClick={closeSubmitStatusDialog}>Close</button>
--- src/components/CardContainer.jsx
+++ src/components/CardContainer.jsx
@@ -2,7 +2,26 @@ import Card from "./Card";
 import './CardContainer.css';
 import PropTypes from 'prop-types';
 
-const CardContainer = ({cardData}) => {
+const CardContainer = ({cardData, sortValue, sortOrder}) => {
+    // const sortedCardData = (data) => {
+    //     switch(sortValue) {
+    //         case "id":
+    //             return data.sort((a, b) => {
+    //                 return sortOrder === "asc" ? a.id - b.id : b.id - a.id;
+    //             });
+    //         case "likes":
+    //             return data.sort((a, b) => {
+    //                 return sortOrder === "asc" ? a.likes - b.likes : b.likes - a.likes;
+    //             });
+    //         case "text":
+    //             return data.sort((a, b) => {
+    //                 return sortOrder === "asc" ? a.text.localeCompare(b.text) : b.text.localeCompare(a.text);
+    //             });
+    //         default:
+    //             return data;
+    //     }
+    // };
+
     const cards = cardData.map((card) => {
         return(
             <Card key={card.id} text={card.text} likes={card.likes} />
@@ -28,7 +47,9 @@ CardContainer.propTypes = {
         id: PropTypes.number.isRequired,
         text: PropTypes.string.isRequired,
         likes: PropTypes.number.isRequired
-    })).isRequired
+    })).isRequired,
+    sortValue: PropTypes.string.isRequired,
+    sortOrder: PropTypes.string.isRequired
 };
 
 
